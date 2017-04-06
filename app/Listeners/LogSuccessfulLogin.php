5bb7e89826c8ce38f180bdb7deecc34ca6a16076
Blaine Jones <blainecjones@gmail.com>
2017-04-06 01:24:11 -0700

    login log

--- app/Listeners/LogSuccessfulLogin.php
+++ app/Listeners/LogSuccessfulLogin.php
@@ -29,6 +29,6 @@ class LogSuccessfulLogin
         //
         $event->user->last_login = date('Y-m-d H:i:s');
         $event->user->save();
-        \App\Log::user($event->user->id,'login');
+        \App\Log::loguser($event->user->id,'login');
     }
 }
