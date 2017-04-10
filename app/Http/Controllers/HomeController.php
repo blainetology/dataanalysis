4996b064ba8bf30caea11ec4db6f72000d7e91a1
Blaine Jones <blainecjones@gmail.com>
2017-04-09 19:31:22 -0700

    changed activity timestamp to Phoenix timezone

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,6 +19,7 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        \DB::statement('SET time_zone  = "America/Phoenix"');
     }
 
     /**
