b877a644a903ebfce0d68bd79f8d973248612cb5
Blaine Jones <blainecjones@gmail.com>
2017-04-22 00:35:00 -0700

    reports admin partials

--- resources/views/admin/reports/includes/total_amt_issued_source.blade.php
+++ resources/views/admin/reports/includes/total_amt_issued_source.blade.php
@@ -1,14 +1,29 @@
 <div class="row">
 	<div class="col-md-4">
-		<label>Spreadsheet ID</label><br/>
-		{{ Form::text('spreadsheet ID',(!empty($input['rules']) ? $input['rules']['spreadsheet'] : null),['name'=>'rules[spreadsheet]', 'class'=>'form-control'])}}
+		<label>Production Spreadsheet ID</label><br/>
+		{{ Form::text('Spreadsheet ID',(!empty($input['rules']) ? $input['rules']['spreadsheet'] : null),['name'=>'rules[spreadsheet]', 'class'=>'form-control'])}}
 	</div>
 	<div class="col-md-4">
-		<label>Set Column</label><br/>
-		{{ Form::text('Set Column',(!empty($input['rules']) ? $input['rules']['set'] : null),['name'=>'rules[set]', 'class'=>'form-control'])}}
+		<label>Date Column</label><br/>
+		{{ Form::text('Date Column',(!empty($input['rules']) ? $input['rules']['date'] : null),['name'=>'rules[date]', 'class'=>'form-control'])}}
 	</div>
 	<div class="col-md-4">
-		<label>Kept Column</label><br/>
-		{{ Form::text('Kept Column',(!empty($input['rules']) ? $input['rules']['kept'] : null),['name'=>'rules[kept]', 'class'=>'form-control'])}}
+		<label>Month Column</label><br/>
+		{{ Form::text('Month Column',(!empty($input['rules']) ? $input['rules']['month'] : null),['name'=>'rules[month]', 'class'=>'form-control'])}}
+	</div>
+</div>
+<br/>
+<div class="row">
+	<div class="col-md-3">
+		<label>Source Column</label><br/>
+		{{ Form::text('Source Column',(!empty($input['rules']) ? $input['rules']['source'] : null),['name'=>'rules[source]', 'class'=>'form-control'])}}
+	</div>
+	<div class="col-md-3">
+		<label>FIA Column</label><br/>
+		{{ Form::text('FIA Column',(!empty($input['rules']) ? $input['rules']['fia'] : null),['name'=>'rules[fia]', 'class'=>'form-control'])}}
+	</div>
+	<div class="col-md-3">
+		<label>AUM Column</label><br/>
+		{{ Form::text('AUM Column',(!empty($input['rules']) ? $input['rules']['aum'] : null),['name'=>'rules[aum]', 'class'=>'form-control'])}}
 	</div>
 </div>
--- resources/views/admin/reports/includes/total_amt_pending.blade.php
+++ resources/views/admin/reports/includes/total_amt_pending.blade.php
@@ -1,14 +1,25 @@
 <div class="row">
 	<div class="col-md-4">
-		<label>Spreadsheet ID</label><br/>
-		{{ Form::text('spreadsheet ID',(!empty($input['rules']) ? $input['rules']['spreadsheet'] : null),['name'=>'rules[spreadsheet]', 'class'=>'form-control'])}}
+		<label>Production Spreadsheet ID</label><br/>
+		{{ Form::text('Spreadsheet ID',(!empty($input['rules']) ? $input['rules']['spreadsheet'] : null),['name'=>'rules[spreadsheet]', 'class'=>'form-control'])}}
 	</div>
 	<div class="col-md-4">
-		<label>Set Column</label><br/>
-		{{ Form::text('Set Column',(!empty($input['rules']) ? $input['rules']['set'] : null),['name'=>'rules[set]', 'class'=>'form-control'])}}
+		<label>Date Column</label><br/>
+		{{ Form::text('Dat Column',(!empty($input['rules']) ? $input['rules']['date'] : null),['name'=>'rules[date]', 'class'=>'form-control'])}}
 	</div>
-	<div class="col-md-4">
-		<label>Kept Column</label><br/>
-		{{ Form::text('Kept Column',(!empty($input['rules']) ? $input['rules']['kept'] : null),['name'=>'rules[kept]', 'class'=>'form-control'])}}
+</div>
+<br/>
+<div class="row">
+	<div class="col-md-3">
+		<label>Written Column</label><br/>
+		{{ Form::text('Advisor Column',(!empty($input['rules']) ? $input['rules']['written'] : null),['name'=>'rules[written]', 'class'=>'form-control'])}}
+	</div>
+	<div class="col-md-3">
+		<label>Conditional Column</label><br/>
+		{{ Form::text('Conditional Column',(!empty($input['rules']) ? $input['rules']['conditional'] : null),['name'=>'rules[conditional]', 'class'=>'form-control'])}}
+	</div>
+	<div class="col-md-3">
+		<label>Condiional Value</label><br/>
+		{{ Form::text('Condiional Value',(!empty($input['rules']) ? $input['rules']['value'] : null),['name'=>'rules[value]', 'class'=>'form-control'])}}
 	</div>
 </div>
